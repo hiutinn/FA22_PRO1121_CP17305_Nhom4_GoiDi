b1e9e9ba5af51b8eaba5253008a5435e16be7cea
HiuTin <tienhieun5@gmail.com>
2022-11-09 21:15:06 +0700

    "Thêm màn hình đăng nhập và chức năng đăng xuất - Híu"

--- app/src/main/java/hieuntph22081/fpoly/goidi/MainActivity.java
+++ app/src/main/java/hieuntph22081/fpoly/goidi/MainActivity.java
@@ -8,6 +8,8 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
@@ -40,7 +42,18 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        return false;
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.dangXuat:
+                // Cần chỉnh sửa câu này !!!!!!!!
+                savePreference("admin","admin",true,true);
+                // ==================
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+        }
+        return true;
     }
 
     public void replaceFragment(Fragment fragment) {
@@ -58,4 +71,17 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
     }
 
+    void savePreference(String ma, String pw, boolean isLogout, boolean status) {
+        SharedPreferences s = getSharedPreferences("user_information",MODE_PRIVATE);
+        SharedPreferences.Editor editor = s.edit();
+        if (!status) { // Khong luu
+            editor.clear();
+        } else { // luu
+            editor.putString("U",ma);
+            editor.putString("P",pw);
+            editor.putBoolean("isLogout", isLogout);
+            editor.putBoolean("CHK",status);
+        }
+        editor.commit();
+    }
 }
